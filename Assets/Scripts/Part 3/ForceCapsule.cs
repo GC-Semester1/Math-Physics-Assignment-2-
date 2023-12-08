c992cd26643997da07fea65c12e20ef308429ed5
JoeyBamboozles <joemcphee777@hotmail.com>
2023-12-08 17:23:44 -0500

    Math Assignment 2 Completed

--- Assets/Scripts/Part 3/ForceCapsule.cs
+++ Assets/Scripts/Part 3/ForceCapsule.cs
@@ -5,28 +5,32 @@ public class ForceCapsule : MonoBehaviour
     public float mass = 30f;
     public float gravity = -9.8f;
     public float stopPosition = -0.45f;
-    public float impulseForce = 500f;
+    public float impulseForce = 10000f;
     private Vector3 velocity = Vector3.zero;
 
 
     void Update()
     {
-        // Determines if mouse is clicked
         if (Input.GetMouseButtonDown(0))
         {
-            // Applys an impulse force upwards
-            velocity += Vector3.up * impulseForce / mass;
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+            Rect objectRect = new Rect(objectPosition.x - 35f, objectPosition.y - 35f, 50f, 75f);
+
+            if (objectRect.Contains(mousePosition))
+            {
+                velocity += Vector3.up * impulseForce / mass;
+            }
         }
 
-        // Applys gravity
-        Vector3 gravityForce = new Vector2(0f, gravity * mass);
+        Vector3 gravityForce = new Vector3(0f, gravity * mass, 0f);
         velocity += gravityForce * Time.deltaTime;
         transform.position += velocity * Time.deltaTime;
 
         if (transform.position.y <= stopPosition)
         {
             velocity = Vector3.zero;
-
             transform.position = new Vector2(transform.position.x, stopPosition);
         }
     }
--- Assets/Scripts/Part 3/ForceSquare.cs
+++ Assets/Scripts/Part 3/ForceSquare.cs
@@ -1,32 +1,41 @@
 using UnityEngine;
 
-public class ForceSquare: MonoBehaviour
+public class ForceSquare : MonoBehaviour
 {
     public float mass = 10f;
     public float gravity = -9.8f;
     public float stopPosition = -0.95f;
-    public float impulseForce = 500f;
+    public float impulseForce = 1500f;
     private Vector3 velocity = Vector3.zero;
-   
 
     void Update()
     {
-        // Determines if mouse is clicked
+        // Checks for mouse click
         if (Input.GetMouseButtonDown(0))
         {
-            // Applys an impulse force upwards
-            velocity += Vector3.up * impulseForce / mass;
+            // Checks if the mouse click is within a certain range of the primitive's position 
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+                                                   //The other two values represent the width and height of the primitive
+            Rect objectRect = new Rect(objectPosition.x - 35f, objectPosition.y - 35f, 50f, 50f); 
+
+            //Checks to see if mouse in in a close enough range of a primitive to apply the impulse force to that specific one
+            if (objectRect.Contains(mousePosition))
+            {
+                // Applies an impulse force upwards
+                velocity += Vector3.up * impulseForce / mass;
+            }
         }
 
-        // Applys gravity
-        Vector3 gravityForce = new Vector2(0f, gravity * mass);
+        // Applies gravity
+        Vector3 gravityForce = new Vector3(0f, gravity * mass, 0f);
         velocity += gravityForce * Time.deltaTime;
         transform.position += velocity * Time.deltaTime;
 
-       if (transform.position.y <= stopPosition)
+        // Checks for stopPosition
+        if (transform.position.y <= stopPosition)
         {
-           velocity = Vector3.zero;
-
+            velocity = Vector3.zero;
             transform.position = new Vector2(transform.position.x, stopPosition);
         }
     }
